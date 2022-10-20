1567a0b4d71506e4555f554f1018c2a34390324b
DitherWither <demonslayervardhan@gmail.com>
2022-10-20 20:55:02 +0530

    Made fields of common::Patient public, and made a to_string() method for common::Gender

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,12 +1,13 @@
-
 #[derive(Debug)]
 pub struct Patient {
-    id: u32,
-    first_name: String,
-    last_name: String,
-    age: i32,
-    gender: Gender,
-    address: String,
+    /// Set to 0 if id is unknown, when inserting into database,
+    /// It will be generated automatically
+    pub id: u32,
+    pub first_name: String,
+    pub last_name: String,
+    pub age: i32,
+    pub gender: Gender,
+    pub address: String,
 }
 
 #[derive(Debug)]
@@ -14,4 +15,13 @@ pub enum Gender {
     Male,
     Female,
     Other(String),
+}
+impl Gender {
+    pub fn to_string(&self) -> String {
+        match self {
+            Self::Male => String::from("MALE"),
+            Self::Female => String::from("FEMALE"),
+            Self::Other(text) => text.to_string(),
+        }
+    }
 }
