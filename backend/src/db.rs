7147b1431ee4190e2f85c12199d7c1446cd188f4
DitherWither <demonslayervardhan@gmail.com>
2022-10-20 20:57:02 +0530

    Created insert_into_patients() method

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,4 +1,5 @@
 use color_eyre::eyre::Result;
+use common::Patient;
 
 pub struct Database {
     connection: sqlite::Connection,
@@ -19,7 +20,7 @@ impl Database {
     fn create_table_patients(&mut self) -> Result<()> {
         self.connection.execute(
             "CREATE TABLE IF NOT EXISTS patients (
-                id INTEGER PRIMARY KEY,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 first_name TEXT NOT NULL,
                 last_name TEXT,
                 age INTEGER,
@@ -28,6 +29,38 @@ impl Database {
             )",
             [],
         )?;
+        Ok(())
+    }
+
+    /// Inserts a Patient into the patients table
+    /// If the id field of the Patient is 0, A id will be generated automatically
+    pub fn insert_into_patients(&mut self, patient: &Patient) -> Result<()> {
+        if patient.id == 0 {
+            self.connection.execute(
+                "INSERT INTO patients (first_name, last_name, age, gender, address)
+                    VALUES (?1, ?2, ?3, ?4, ?5)",
+                (
+                    &patient.first_name,
+                    &patient.last_name,
+                    &patient.age,
+                    &patient.gender.to_string(),
+                    &patient.address,
+                ),
+            )?;
+        } else {
+            self.connection.execute(
+                "INSERT OR REPLACE INTO patients (id, first_name, last_name, age, gender, address)
+                    VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+                (
+                    &patient.id,
+                    &patient.first_name,
+                    &patient.last_name,
+                    &patient.age,
+                    &patient.gender.to_string(),
+                    &patient.address,
+                ),
+            )?;
+        }
         Ok(())
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,24 @@
 mod db;
 
 use color_eyre::eyre::Result;
+use common::*;
 use db::Database;
 
 fn main() -> Result<()> {
     // Installs the color_eyre panic and error report hanlers
     color_eyre::install()?;
 
-    let db = Database::init()?;
+    let mut db = Database::init()?;
+    let patient = Patient {
+        id: 1,
+        first_name: "Turrdhan".to_string(),
+        last_name: "Chatil".to_string(),
+        age: 15,
+        gender: Gender::Male,
+        address: "fr, who knows".to_string(),
+    };
+
+    db.insert_into_patients(&patient)?;
 
     Ok(())
 }
